5f84dbbf74c1564827c5c3b8eb6ec1add5853d2e
len201 <cctnsw@gmail.com:q!:wq>
2021-02-14 20:14:27 -0500

    class

--- ES6/classes/classes.ts
+++ ES6/classes/classes.ts
@@ -1,9 +1,20 @@
 
 //class: 首字母大写
+// class Vehicle {
+//   color: string;
+
+//   constructor(color: string) {
+//     this.color = color;
+//   }
+// 可简化成以下：
 class Vehicle {
+    constructor(public color: string) {
+      this.color = color;
+    }
+
   //no need to write "function"
   drive() {
-    return 'vroom';
+    console.log('chugga chugga');
   }
 
   honk(): void {
@@ -11,7 +22,23 @@ class Vehicle {
   }
 }
 
+//inherit
+class Car extends Vehicle {
+  //override: modifier (public/private/protect) must be the same as the func in parent
+  constructor(public wheels: number, color: string){ //color前不要再写modifier，因为继承父类的field
+    super(color);
+  }
+
+  drive(){
+    console.log('vroom');
+  }
+
+  startDrivingProcess(): void {
+    this.drive();
+  }
+  
+}
 //instance首字母小写
-const vehicle = new Vehicle();
-vehicle.drive();
-vehicle.honk();
+// const car = new Car();
+// car.drive(); //vroom
+// car.honk();
